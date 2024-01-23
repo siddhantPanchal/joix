7905bf1e27949f6a22fdf2ee76f08ddcbdf4fc5f
siddhantpanchal430 <siddhantpanchal430@gmail.com>
2024-01-23 18:25:30 +0000

    removed warnings

--- lib/src/types/interfaces/defaultable.dart
+++ lib/src/types/interfaces/defaultable.dart
@@ -1,5 +1,3 @@
-import '../../joix_base.dart';
-
 abstract interface class Defaultable<T> {
   void defaultValue(T value);
 }
--- lib/src/types/interfaces/measurable.dart
+++ lib/src/types/interfaces/measurable.dart
@@ -1,5 +1,3 @@
-import '../../joix_base.dart';
-
 abstract interface class Limitable<T> {
   void limit(int length);
 }
