88cdde188d3e9ab24caadd263f3e95adb04fecf1
siddhantPanchal <siddhantPanchal430@gmail.com>
2024-02-09 15:20:10 +0000

    updated tests

--- test/src/types/map_test.dart
+++ test/src/types/map_test.dart
@@ -63,48 +63,28 @@ void main() {
 
         test("validate the map value when valid values are passed in map value",
             () {
-          final schema = JoiX.object({
-            'name': JoiX.string()
-              ..max(10)
-              ..required(),
-            'age': JoiX.num()..int(),
-            'gender': JoiX.string()
-              ..valid(['male', 'female', 'other'], caseSensitive: false)
-              ..required(),
-            'city': JoiX.string(),
-            'country': JoiX.string()..countryCode(),
-            'email': JoiX.string()
-              ..email()
-              ..required(),
-            'phone': JoiX.string()
-              ..match(RegExp(r'\+\d+'))
-              ..required(),
-            'occupation': JoiX.string(),
-            'hobby': JoiX.string()
-              ..valid(["reading", "writing", "swimming", "ridding"]),
-            'favorite_color': JoiX.string()
-              ..valid(["blue", "green", "yellow", "red", "violet"]),
-            "password": JoiX.string()..required(),
+          final schema = joi.object({
             "confirm_password": JoiX.ref("password"),
+            "password": JoiX.string()..required(),
           });
 
-          var map = {
-            'name': 'Alice',
-            'age': 25,
-            'gender': 'female',
-            'city': 'Mumbai',
-            'country': 'IN',
-            'email': 'alice@gmail.com',
-            'phone': '+91-9876543210',
-            'occupation': 'software engineer',
-            'hobby': 'reading',
-            'favorite_color': 'blue',
-            "password": "abc",
-            "confirm_password": "abcd"
-          };
+          var map = {"password": "abc", "confirm_password": "abc"};
           var result = schema.validate(value: map);
           print(result);
           checkForPass(result);
+        });
+        test(
+            "validate the map value when invalid values are passed in map value",
+            () {
+          final schema = joi.object({
+            "confirm_password": JoiX.ref("password"),
+            "password": JoiX.string()..required(),
+          });
+
+          var map = {"password": "abc", "confirm_password": "abcd"};
+          var result = schema.validate(value: map);
+          checkForFail(
+              result, "confirm_password value is not same as password");
         });
 
         test(
